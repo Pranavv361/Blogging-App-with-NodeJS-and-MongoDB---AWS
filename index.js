e1e085b500330384a4d93254cdc7959674dc8458
Pranav Vichare <pranavvichare361@gmail.com>
2024-08-16 17:38:28 -0500

    Add Partials and Dev Dependencies

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 8000;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 app.listen(PORT, () => console.log("Server started at ", PORT));
 
 console.log("Starting the servers.. ");

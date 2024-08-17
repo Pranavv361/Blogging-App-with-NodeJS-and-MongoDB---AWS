3b574ae1f027ab0870d4a236f15b977d44dd5d6b
Pranav Vichare <pranavvichare361@gmail.com>
2024-08-17 10:07:59 -0500

    Update index and Partials

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const userRoute = require("./routes/user");
 const app = express();
 const PORT = 8000;
+
+mongoose.connect('mongodb://localhost:27017/blogit!').then(e=>console.log("Mongodb connected"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
